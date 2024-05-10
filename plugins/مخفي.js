f7610c37c5069432c8d13ed76ead3b59c3ed64c4
zyad5yasser <zyadyasser909@gmail.com>
2024-05-10 15:57:32 +0300

    Update مخفي.js

--- plugins/مخفي.js
+++ plugins/مخفي.js
@@ -1,26 +1,27 @@
-import MessageType from '@adiwajshing/baileys'
-import { generateWAMessageFromContent } from '@adiwajshing/baileys'
+import fs from 'fs'
+import fetch from 'node-fetch'
+let handler = async(m, { conn, text, participants }) => {
+const fkontak = {
+  "key": {
+    "participants":"0@s.whatsapp.net",
+    "remoteJid": "status@broadcast",
+    "fromMe": false,
+    "id": "Halo"
+  },
+  "message": {
+    "contactMessage": {
+      "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+    }
+  },
+  "participant": "0@s.whatsapp.net"
+}
 
-let handler = async (m, { conn, text, participants }) => {
-let users = participants.map(u => conn.decodeJid(u.id))
-let q = m.quoted ? m.quoted : m
-let c = m.quoted ? m.quoted : m.msg
-const msg = conn.cMod(m.chat,
-generateWAMessageFromContent(m.chat, {
-[c.toJSON ? q.mtype : 'extendedTextMessage']: c.toJSON ? c.toJSON() : {
-text: c || ''
-}
-}, {
-quoted: m,
-userJid: conn.user.id
-}),
-text || q.text, conn.user.jid, { mentions: users }
-)
-await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
-}
-handler.help = ['hidetag']
+    conn.sendMessage(m.chat, { text: text, mentions: participants.map(a => a.id) }, {quoted:fkontak})
+    }
+handler.help = ['مخفي']
 handler.tags = ['group']
-handler.command = ['مخفي'] 
+handler.command = /^(مخفي)$/i
+
 handler.group = true
 handler.admin = true
 
