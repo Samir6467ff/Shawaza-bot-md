30937af7b26af313b2e061f4b9652d142981eb0f
zyad5yasser <zyadyasser909@gmail.com>
2024-05-10 14:15:12 +0300

    Update اكس3.js

--- plugins/اكس3.js
+++ plugins/اكس3.js
@@ -4,6 +4,6 @@ let room = Object.values(conn.game).find(room => room.id.startsWith('tictactoe')
 if (room == undefined) return conn.sendButton(m.chat, '*[❗] انت لست في لعبه تيك تاك تو (اكس او)*', wm, null, [['ابدأ غرفه جديدة', `${usedPrefix}ttt مباراه جديدة`]], m)
 delete conn.game[room.id]
 await m.reply('*[ ✔ ] تمت ازاله الغرفه*')}
-handler.command = /^(delttt|deltt|delxo|deltictactoe)$/i
+handler.command = /^(delttt|deltt|delxo|deltictactoe|حذف_اكس|حذفاكس)$/i
 handler.fail = null
 export default handler
