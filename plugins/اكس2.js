e89008b399af29f5ee0add45e9b4b35ea84da0b9
zyad5yasser <zyadyasser909@gmail.com>
2024-05-10 14:10:54 +0300

    Update اكس2.js

--- plugins/اكس2.js
+++ plugins/اكس2.js
@@ -15,7 +15,7 @@ export async function before(m) {
     let room = Object.values(this.game).find(room => room.id && room.game && room.state && room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender) && room.state == 'PLAYING')
     if (room) {
         // m.reply(`[DEBUG]\n${parseInt(m.text)}`)
-        if (!/^([1-9]|(me)?nyerah|surr?ender)$/i.test(m.text))
+        if (!/^([1-9]|(me)?nyerah|surr?ender|انسحب)$/i.test(m.text))
             return !0
         isSurrender = !/^[1-9]$/.test(m.text)
         if (m.sender !== room.game.currentTurn) { // nek wayahku
