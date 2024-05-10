055b6f591c4d3c202d44e019f3fa824caaf5d708
zyad5yasser <zyadyasser909@gmail.com>
2024-05-11 02:05:24 +0300

    Update تطقيم.js

--- plugins/تطقيم.js
+++ plugins/تطقيم.js
@@ -1,16 +1,24 @@
-import fetch from "node-fetch"
-let handler = async (m, { conn }) => {
+import fetch from "node-fetch";
 
-  let data = await (await fetch('https://raw.githubusercontent.com/KazukoGans/database/main/anime/ppcouple.json')).json()
-  let cita = data[Math.floor(Math.random() * data.length)]
-  
-  let cowi = await(await fetch(cita.cowo)).buffer()
-  await conn.sendFile(m.chat, cowi, '', '♂️','*للولد*', m)
-  let ciwi = await(await fetch(cita.cewe)).buffer()
-  await conn.sendFile(m.chat, ciwi, '', '♀️','*للبنت*', m)
-}
-handler.help = ['ppcouple', 'ppcp']
-handler.tags = ['img']
-handler.command = ['couplepp','كابلز','تطقيم'] 
-contact.fromme = true
-export default handler
+let handler = async (m, { conn }) => {
+  try {
+    let data = await (await fetch('https://raw.githubusercontent.com/KazukoGans/database/main/anime/ppcouple.json')).json();
+    let cita = data[Math.floor(Math.random() * data.length)];
+    
+    let cowi = await (await fetch(cita.cowo)).buffer();
+    await conn.sendFile(m.chat, cowi, '', '♂️', '*للولد*', m);
+    
+    let ciwi = await (await fetch(cita.cewe)).buffer();
+    await conn.sendFile(m.chat, ciwi, '', '♀️', '*للبنت*', m);
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
+
+handler.help = ['ppcouple', 'ppcp'];
+handler.tags = ['img'];
+handler.command = ['couplepp', 'كابلز', 'تطقيم'];
+handler.fromMe = true;
+
+export default handler;
