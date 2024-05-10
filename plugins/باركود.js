817778d1fbf85f3c92103d463742da5da2f34484
zyad5yasser <zyadyasser909@gmail.com>
2024-05-10 15:47:17 +0300

    Update باركود.js

--- plugins/باركود.js
+++ plugins/باركود.js
@@ -1,6 +1,6 @@
 import { toDataURL } from 'qrcode'
 let handler = async (m, { text, conn }) => {
-if (!text) throw `*حط اانص االي عايز تحولو لكود بعد الامر\n\nمثال\n\n.كود زيزو بوت*`
+if (!text) throw `*حط النص االي عايز تحولو لكود بعد الامر*\n\n*مثال*\n\n*.كود زيزو بوت*`
 conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', 'BY:𝑧ₑ𝑧ₒ_𝑏ₒ𝑡', m)
 }
 handler.help = ['', 'code'].map(v => 'qr' + v + ' <teks>')
