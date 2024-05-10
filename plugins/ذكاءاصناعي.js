48626a18c34e6648126419676a61643bde76ecd7
zyad5yasser <zyadyasser909@gmail.com>
2024-05-11 01:11:51 +0300

    Create ذكاءاصناعي.js

---
+++ plugins/ذكاءاصناعي.js
@@ -0,0 +1,76 @@
+import fetch from 'node-fetch';
+
+let handler = async (m, { text, conn, usedPrefix, command }) => {
+  if (!text && !(m.quoted && m.quoted.text)) {
+    throw `الرجاء تقديم نص أو اي شئ برسالة للحصول على رد.`;
+  }
+
+  if (!text && m.quoted && m.quoted.text) {
+    text = m.quoted.text;
+  }
+
+  try {
+    m.react(rwait)
+    const { key } = await conn.sendMessage(m.chat, {
+      image: { url: 'https://telegra.ph/file/abafb0983183373f95cf4.png' },
+      caption: 'سيبني افكر....'
+    }, {quoted: m})
+    conn.sendPresenceUpdate('composing', m.chat);
+    const prompt = encodeURIComponent(text);
+
+    const guru1 = `${gurubot}/chatgpt?text=${prompt}`;
+    
+    try {
+      let response = await fetch(guru1);
+      let data = await response.json();
+      let result = data.result;
+
+      if (!result) {
+        
+        throw new Error('لا يوجد استجابة JSON صالحة من الAPI الأول');
+      }
+
+      await conn.relayMessage(m.chat, {
+        protocolMessage: {
+          key,
+          type: 14,
+          editedMessage: {
+            imageMessage: { caption: result }
+          }
+        }
+      }, {});
+      m.react(done);
+    } catch (error) {
+      console.error('خطأ من الAPI الأول:', error);
+
+  
+      const model = 'llama';
+      const senderNumber = m.sender.replace(/[^0-9]/g, ''); 
+      const session = `GURU_BOT_${senderNumber}`;
+      const guru2 = `https://ultimetron.guruapi.tech/gpt3?prompt=${prompt}`;
+      
+      let response = await fetch(guru2);
+      let data = await response.json();
+      let result = data.completion;
+
+      await conn.relayMessage(m.chat, {
+        protocolMessage: {
+          key,
+          type: 14,
+          editedMessage: {
+            imageMessage: { caption: result }
+          }
+        }
+      }, {});
+      m.react(done);
+    }
+
+    } catch {
+        throw `*[❗] خطأ، يرجى إدخال نص صحيح*`;
+  }
+};
+handler.help = ['chatgpt']
+handler.tags = ['AI']
+handler.command = ['يحب', 'chatgpt', 'ai', 'gpt','زيزو'];
+
+export default handler;
