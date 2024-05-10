bc35db6788e608376d22503565f37ec6f23e9b15
zyad5yasser <zyadyasser909@gmail.com>
2024-05-10 13:49:58 +0300

    Update Chatbot.js

--- plugins/Chatbot.js
+++ plugins/Chatbot.js
@@ -3,7 +3,7 @@ let handler = m => m;
  handler.all = async function (m) { 
  let chat = global.db.data.chats[m.chat]; 
  let responses; 
- if (/^ميجو$/i.test(m.text)) { 
+ if (/^زيزو$/i.test(m.text)) { 
  responses = [ 
  'تحت امرك حبي'
  ]; 
