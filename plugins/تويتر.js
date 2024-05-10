7299bff0c0b6260ced36228aa44baa58e2562a56
zyad5yasser <zyadyasser909@gmail.com>
2024-05-11 01:24:15 +0300

    Update تويتر.js

--- plugins/تويتر.js
+++ plugins/تويتر.js
@@ -2,7 +2,7 @@
 import fg from 'api-dylux'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 if (!args[0]) throw `📌 المثال: \n*${usedPrefix + command}* https://twitter.com/fernandavasro/status/1569741835555291139?t=ADxk8P3Z3prq8USIZUqXCg&s=19`
-          m.react(rwait)    
+          m.reply(wait)    
           try {
           let { SD, HD, desc, thumb, audio } = await fg.twitter(args[0])
           let te = ` 
