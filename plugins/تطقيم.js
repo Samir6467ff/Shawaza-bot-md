641ed098ce7a4539d7b768f0da9c673e7fc35292
zyad5yasser <zyadyasser909@gmail.com>
2024-05-11 03:18:33 +0300

    Update تطقيم.js

--- plugins/تطقيم.js
+++ plugins/تطقيم.js
@@ -4,10 +4,10 @@ let handler = async (m, { conn }) => {
   let data = await (await fetch('https://raw.githubusercontent.com/KazukoGans/database/main/anime/ppcouple.json')).json()
   let cita = data[Math.floor(Math.random() * data.length)]
   
-  let man = await(await fetch(cita.male)).buffer()
-  await conn.sendFile(m.chat, man, '', '🤵🏻ولد\n𝐵𝑌:𝑧ₑ𝑧ₒ_𝑏ₒ𝑡', m)
-  let girl = await(await fetch(cita.female)).buffer()
-  await conn.sendFile(m.chat, girl, '', '👰🏻‍♀️بنت\n𝐵𝑌:𝑧ₑ𝑧ₒ_𝑏ₒ𝑡', m)
+  let cowo = await(await fetch(cita.male)).buffer()
+  await conn.sendFile(m.chat, cowo, '', '🤵🏻ولد\n𝐵𝑌:𝑧ₑ𝑧ₒ_𝑏ₒ𝑡', m)
+  let cewe = await(await fetch(cita.female)).buffer()
+  await conn.sendFile(m.chat, cewe, '', '👰🏻‍♀️بنت\n𝐵𝑌:𝑧ₑ𝑧ₒ_𝑏ₒ𝑡', m)
 }
 handler.help = ['ppcouple', 'ppcp']
 handler.tags = ['t2m']
