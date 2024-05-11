db85877d722e0893a9d529e2c7955d1bab0fd726
zyad5yasser <zyadyasser909@gmail.com>
2024-05-11 03:48:35 +0300

    Update تطقيم.js

--- plugins/تطقيم.js
+++ plugins/تطقيم.js
@@ -6,9 +6,9 @@ let handler = async (m, { conn }) => {
   let cita = data[Math.floor(Math.random() * data.length)]
 
   let cowi = await (await fetch(cita.cowo)).buffer()
-  await conn.sendFile(m.chat, cowi, '', '♂️', m)
+  await conn.sendFile(m.chat, cowi, '', '*للولد ♂️*', m)
   let ciwi = await (await fetch(cita.cewe)).buffer()
-  await conn.sendFile(m.chat, ciwi, '', '♀️', m)
+  await conn.sendFile(m.chat, ciwi, '', '*للبنت ♀️*', m)
 }
 handler.help = ['ppcouple', 'ppcp']
 handler.tags = ['img']
