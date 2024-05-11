534c5af5efdbc1bbfe1090ef4b4e1162abb0c088
zyad5yasser <zyadyasser909@gmail.com>
2024-05-11 09:23:38 +0300

    Update جبيتي4.js

--- plugins/جبيتي4.js
+++ plugins/جبيتي4.js
@@ -1,56 +1,28 @@
-
+import {GoogleGenerativeAI} from '@google/generative-ai'
 import displayLoadingScreen from '../lib/loading.js'
-import fetch from 'node-fetch'
-import {delay} from '@whiskeysockets/baileys'
-import translate from '@vitalets/google-translate-api';
+const genAI = new GoogleGenerativeAI('AIzaSyDJC5a882ruaC4XL6ejY1yhgRkN-JNQKg8');
+
 
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
   try {
-    if (!text) throw `امم.. ماذا تريد أن تقول؟`
-    await conn.sendMessage(m.chat, { react: { text: '🤖', key: m.key } })
+    if (!text) throw `هممم.. ماذا تريد أن تقول؟`
+    m.react('🤖')
+    await displayLoadingScreen(conn, m.chat)
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
-    //await displayLoadingScreen(conn, m.chat)
+    const prompt = text
 
-
-    const prompt = encodeURIComponent(text);
-    let apiurl = `https://ultimetron.guruapi.tech/gpt4?prompt=${prompt}`
-
-    const result = await fetch(apiurl);
-    const response = await result.json();
-    console.log(response)
-    const englishText = response.result.reply;
-
-    // ترجمة النص إلى اللغة العربية
-    const arabicTranslation = await translate(englishText, { to: 'ar' });
-
-    await typewriterEffect(conn, m, m.chat, arabicTranslation.text);
-       
+    const result = await model.generateContent(prompt);
+    const response = result.response;
+    const textt = response.text();
+    m.reply(textt)
   } catch (error) {
     console.error(error);
-    m.reply('أُووبس! هناك خطأ ما. ، ونحن نحاول إصلاحه في أسرع وقت ممكن');
+    m.reply('عذرًا! حدث خطأ ما. نحن نبذل قصارى جهدنا لإصلاحه في أقرب وقت ممكن.');
   }
 }
 handler.help = ['gemini <text>']
 handler.tags = ['tools']
-handler.command = /^(gpt4|جيبيتي|جيبي_تي)$/i
+handler.command = /^(gemini|جميني|بوت)$/i
 
 export default handler
-
-async function typewriterEffect(conn, quoted ,from, text) {
-    let { key } = await conn.sendMessage(from, { text: 'Thinking...' } , {quoted:quoted})
-  
-    for (let i = 0; i < text.length; i++) {
-      const noobText = text.slice(0, i + 1);
-      await conn.relayMessage(from, {
-        protocolMessage: {
-          key: key,
-          type: 14,
-          editedMessage: {
-            conversation: noobText
-          }
-        }
-      }, {});
-   
-       await delay(100); // Adjust the delay time (in milliseconds) as needed
-    }
-}
