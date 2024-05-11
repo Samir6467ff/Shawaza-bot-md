e01c425f03e8038b5c012bc2d60cdbe01282560f
zyad5yasser <zyadyasser909@gmail.com>
2024-05-11 05:10:16 +0300

    Update نشر.js

--- plugins/نشر.js
+++ plugins/نشر.js
@@ -2,16 +2,31 @@
 import { randomBytes } from 'crypto'
 
 let handler = async (m, { conn, text }) => {
-  let chats = Object.entries(conn.chats).filter(([_, chat]) => chat.isChats).map(v => v[0])
+  let chats = Object.entries(conn.chats)
+    .filter(([_, chat]) => chat.isChats)
+    .map(v => v[0])
   let cc = conn.serializeM(text ? m : m.quoted ? await m.getQuotedObj() : false || m)
   let teks = text ? text : cc.text
-  conn.reply(m.chat, `جاري التجهيز *عدد الجروبات:* ${chats.length}`, m)
-  for (let id of chats) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast|tx/i.test(teks) ? teks : `${teks}` ), true).catch(_ => _)
-  m.reply('تم التحويل')
+  conn.reply(m.chat, `*✅ اذاعه للكل *عدد المجموعات:* ${chats.length} مجموعه*`, m)
+  for (let id of chats)
+    await conn
+      .copyNForward(
+        id,
+        conn.cMod(
+          m.chat,
+          cc,
+          /bc|broadcast|tx|اذاعه/i.test(teks)
+            ? teks
+            : `*اذاعه ┃ المالك*\n_____________________\n ${teks} `
+        ),
+        true
+      )
+      .catch(_ => _)
+  m.reply('*✅ تم النشر ف كل المجموعات :)*')
 }
 handler.help = ['tx']
 handler.tags = ['owner']
-handler.command = /^(نشر)$/i
+handler.command = /^(broadcast|bc|tx|نشر|اذاعه)$/i
 handler.owner = true
 
 export default handler
@@ -19,4 +34,7 @@ export default handler
 const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
 
-const randomID = length => randomBytes(Math.ceil(length * .5)).toString('hex').slice(0, length)
+const randomID = length =>
+  randomBytes(Math.ceil(length * 0.5))
+    .toString('hex')
+    .slice(0, length)
