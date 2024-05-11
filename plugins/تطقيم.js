57a8203a23ca7c04a672475c5255cca32f568dc2
zyad5yasser <zyadyasser909@gmail.com>
2024-05-11 03:42:51 +0300

    Update تطقيم.js

--- plugins/تطقيم.js
+++ plugins/تطقيم.js
@@ -1,17 +1,23 @@
-import fetch from "node-fetch"
+import fetch from "node-fetch";
+
 let handler = async (m, { conn }) => {
+  try {
+    let data = await (await fetch('./src/game/ppcouple.json')).json();
+    let cita = data[Math.floor(Math.random() * data.length)];
+    
+    let cowo = await (await fetch(cita.male)).buffer();
+    await conn.sendFile(m.chat, cowo, '', '🤵🏻 ولد\n𝐵𝑌: 𝑧ₑ𝑧ₒ_𝑏ₒ𝑡', m);
+    
+    let cewe = await (await fetch(cita.female)).buffer();
+    await conn.sendFile(m.chat, cewe, '', '👰🏻‍♀️ بنت\n𝐵𝑌: 𝑧ₑ𝑧ₒ_𝑏ₒ𝑡', m);
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
 
-  let data = await (await from('./src/game/ppcouple.json')).json()
-  let cita = data[Math.floor(Math.random() * data.length)]
-  
-  let cowo = await(await fetch(cita.male)).buffer()
-  await conn.sendFile(m.chat, cowo, '', '🤵🏻ولد\n𝐵𝑌:𝑧ₑ𝑧ₒ_𝑏ₒ𝑡', m)
-  let cewe = await(await fetch(cita.female)).buffer()
-  await conn.sendFile(m.chat, cewe, '', '👰🏻‍♀️بنت\n𝐵𝑌:𝑧ₑ𝑧ₒ_𝑏ₒ𝑡', m)
-}
-handler.help = ['ppcouple', 'ppcp']
-handler.tags = ['t2m']
-handler.command = ['طقم','تطقيم'] 
+handler.help = ['ppcouple', 'ppcp'];
+handler.tags = ['t2m'];
+handler.command = ['طقم', 'تطقيم'];
 
-
-export default handler
+export default handler;
