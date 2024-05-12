12c85d6aab60f7dff2a8a7061a7406689cbedc91
zyad5yasser <zyadyasser909@gmail.com>
2024-05-12 16:44:11 +0300

    Create كوره.js

---
+++ plugins/كوره.js
@@ -0,0 +1,35 @@
+let timeout = 60000
+let poin = 500
+let handler = async (m, { conn, command, usedPrefix }) => {
+    conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
+    let id = m.chat
+    if (id in conn.tebakbendera) {
+        conn.reply(m.chat, '❐┃لم يتم الاجابة علي السؤال بعد┃❌ ❯', conn.tebakbendera[id][0])
+        throw false
+    }
+    let src = await (await fetch('https://gist.githubusercontent.com/Kyutaka101/3a8aa789b4a5f1a28f1fc09b75e14787/raw/2fe52989ee6c9f87441f8900cc7ea62e1f17a765/gistfile1.txt')).json()
+  let json = src[Math.floor(Math.random() * src.length)]
+    let caption = `*· • • ━━ ⌝🐉⌞ ━━ • • ·*
+*${command.toUpperCase()}*
+  *⌬ ❛╏السـؤال ✍🏻⇜『من الاعب ال بالصورة』*
+  *⌬ ❛╏الـوقـت⏳↞ ${(timeout / 1000).toFixed(2)} ┇*
+  *استخدم .انسحب للأنسحاب*
+  *⌬ ❛╏الـجـائزة💰↞ ${poin} نقاط┇*
+∞┇━━━ •🐉• ━━━┇∞
+*⌬ ❛╏ تــ✍︎ــوقــيــع ↯:*
+*⌬ ❛╏ 𝑧ₑ𝑧ₒ_𝑏ₒ𝑡 ╏*
+     `.trim()
+    conn.tebakbendera[id] = [
+        await conn.sendFile(m.chat, json.img, '', caption, m),
+        json, poin,
+        setTimeout(() => {
+            if (conn.tebakbendera[id]) conn.reply(m.chat, `❮ ⌛┇انتهي الوقت┇⌛❯\n❐↞┇الاجـابـة✅↞ *${json.name}* ┇`, conn.tebakbendera[id][0])
+            delete conn.tebakbendera[id]
+        }, timeout)
+    ]
+}
+handler.help = ['كوره']
+handler.tags = ['fun']
+handler.command = /^كوره/i
+
+export default handler 
