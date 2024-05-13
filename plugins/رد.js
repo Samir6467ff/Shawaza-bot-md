e0e3233abb43fd7aca640dd0d3beb6e29e0ece0a
zyad5yasser <zyadyasser909@gmail.com>
2024-05-13 21:19:52 +0300

    Create رد.js

---
+++ plugins/رد.js
@@ -0,0 +1,32 @@
+let handler = m => m
+handler.all = async function (m) {
+
+    if (m.messageStubType == 9 && m.message) { // Check if it's a sticker message
+        let stickerId = m.message.stickerMessage.fileSha256.toString('base64');
+        let stickerUrl = `https://stickershop.line-scdn.net/stickershop/v1/sticker/${stickerId}/android/sticker.png`;
+        this.sendFile(m.chat, stickerUrl, 'sticker.png', '🤖', m);
+        return true; // Return true to indicate message is handled
+    }
+
+    if (/^بيض$/i.test(m.text) ) {
+      let av = 'https://i.imgur.com/1bQSrvh.jpeg'
+      this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+      return true; // Return true to indicate message is handled
+    }
+
+    if (/^احبك$/i.test(m.text) ) {
+       let av = 'https://i.imgur.com/1bQSrvh.jpeg'
+       this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+       return true; // Return true to indicate message is handled
+    }
+
+    if (/^اسطوري$/i.test(m.text) ) {
+        let av = 'https://i.imgur.com/1bQSrvh.jpeg'
+        this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+        return true; // Return true to indicate message is handled
+    }
+  
+    return false; // Return false if message is not handled
+}
+ 
+export default handler;
