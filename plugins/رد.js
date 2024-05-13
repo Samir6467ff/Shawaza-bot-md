a6bfd8388f53227e70d6e512bfbe0135f04cc5ff
zyad5yasser <zyadyasser909@gmail.com>
2024-05-14 00:59:56 +0300

    Update رد.js

--- plugins/رد.js
+++ plugins/رد.js
@@ -10,19 +10,19 @@ handler.all = async function (m) {
 
     if (/^بيض$/i.test(m.text) ) {
       let av = 'https://i.imgur.com/1bQSrvh.jpeg'
-      this.sendFile(m.chat, av, null, m, true, { type: 'audioMessage', ptt: true })
+      this.sendFile(m.chat, av, 'لا', null, m, true, { type: 'audioMessage', ptt: true })
       return true; // Return true to indicate message is handled
     }
 
     if (/^احبك$/i.test(m.text) ) {
        let av = 'https://i.imgur.com/1bQSrvh.jpeg'
-       this.sendFile(m.chat, av, 'media.علاوي.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+       this.sendFile(m.chat, av, '*لا*', null, m, true, { type: 'audioMessage', ptt: true })
        return true; // Return true to indicate message is handled
     }
 
     if (/^اسطوري$/i.test(m.text) ) {
         let av = 'https://i.imgur.com/1bQSrvh.jpeg'
-        this.sendFile(m.chat, av, 'media.علاوي.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+        this.sendFile(m.chat, av, 'لا', null, m, true, { type: 'audioMessage', ptt: true })
         return true; // Return true to indicate message is handled
     }
   
