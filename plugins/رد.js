0cbc60f0026fa98dc4b9b2bf4f9a7cc0753daeec
zyad5yasser <zyadyasser909@gmail.com>
2024-05-14 01:54:46 +0300

    Update رد.js

--- plugins/رد.js
+++ plugins/رد.js
@@ -9,7 +9,7 @@ handler.all = async function (m) {
     }
 
     if (/^بيض$/i.test(m.text) ) {
-      let av = imagen4
+      let av ='https://telegra.ph/file/76ed55228cd0c70426236.jpg'
       this.sendFile(m.chat, av, 'لا', null, m, true, { type: 'audioMessage', ptt: true })
       return true; // Return true to indicate message is handled
     }
