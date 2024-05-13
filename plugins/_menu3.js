cb94cdedd594c8686d895987aabfb8b3f0b9fb8a
zyad5yasser <zyadyasser909@gmail.com>
2024-05-13 20:01:13 +0300

    Create _menu3.js

---
+++ plugins/_menu3.js
@@ -0,0 +1,68 @@
+import fetch from 'node-fetch';
+const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
+//if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `*[❗] Los comandos +18 están desactivados en este grupo, si es admin y desea activarlos use ${usedPrefix}enable modohorny*`; 
+  try {
+    const pp = imagen4;
+    const vn = './media/La biblia.mp3';
+    const d = new Date(new Date + 3600000);
+    const locale = 'es';
+    const week = d.toLocaleDateString(locale, {weekday: 'long'});
+    const date = d.toLocaleDateString(locale, {day: 'numeric', month: 'long', year: 'numeric'});
+    const _uptime = process.uptime() * 1000;
+    const uptime = clockString(_uptime);
+    const user = global.db.data.users[m.sender];
+    const {money, joincount} = global.db.data.users[m.sender];
+    const {exp, limit, level, role} = global.db.data.users[m.sender];
+    const rtotalreg = Object.values(global.db.data.users).filter((user) => user.registered == true).length;
+    const more = String.fromCharCode(8206);
+    const readMore = more.repeat(850);
+    const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
+    const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
+    const document = doc[Math.floor(Math.random() * doc.length)];
+    const str = `
+*⟣┈┈┈┈┈┈⟢┈┈┈⟣┈┈┈┈┈┈┈⟢*
+*⩥🌐│مرحبا: ${taguser}│🌐⩤*
+*⟣┈┈┈┈┈┈⟢┈┈┈⟣┈┈┈┈┈┈┈⟢*   
+    *⩥🎮│العاب│🎮⩤*
+*⟣┈┈┈┈┈┈⟢┈┈┈⟣┈┈┈┈┈┈┈⟢*
+"│✮ ⃟❓❯ .انمي*
+*│✮ ⃟⚽❯ .رياضه*
+*│✮ ⃟❌❯ .اكس_او*
+*│✮ ⃟🎰❯ .رهان*
+*│✮ ⃟💁🏻‍♂️❯ .شخصية*
+*│✮ ⃟❓❯ .احزر*
+*│✮ ⃟👀❯ .عين*
+*│✮ ⃟🎈❯ .ايموجي*
+*│✮ ⃟⚡❯ .سؤال*
+*│✮ ⃟🎮❯ .كت*
+*│✮ ⃟🤔❯ .خمن*
+*│✮ ⃟🔠❯ .فكك*
+*│✮ ⃟🔡❯ .رتب*
+*│✮ ⃟❔❯ .اجابه*
+*│✮ ⃟❔❯ .دين*
+*⟣┈┈┈┈┈┈⟢┈┈┈⟣┈┈┈┈┈┈┈⟢*`.trim();
+    if (m.isGroup) {
+      await conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: m});
+    //  await conn.sendFile(m.chat, vn, 'La biblia.mp3', null, m, true, {type: 'audioMessage', ptt: true});
+    } else {
+      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+      await conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
+      //await conn.sendFile(m.chat, vn, 'La biblia.mp3', null, m, true, {type: 'audioMessage', ptt: true});
+      await conn.sendMessage(m.chat, { react: { text: '🎮', key: m.key } })
+
+    }
+  } catch {
+    conn.reply(m.chat, '*🐦*', m);
+  }
+};
+handler.command = /^(5)$/i;
+handler.exp = 50;
+handler.fail = null;
+export default handler;
+
+function clockString(ms) {
+  const h = isNaN(ms) ? '--' : Math.floor(ms / 3600000);
+  const m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60;
+  const s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60;
+  return [h, m, s].map((v) => v.toString().padStart(2, 0)).join(':');
+                                                                                                                                                                                                                                                                      }
