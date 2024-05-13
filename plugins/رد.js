1547e72c849ff488893d402181f9271d0dda1f9d
zyad5yasser <zyadyasser909@gmail.com>
2024-05-14 01:49:01 +0300

    Update رد.js

--- plugins/رد.js
+++ plugins/رد.js
@@ -3,19 +3,19 @@ handler.all = async function (m) {
 
     if (m.messageStubType == 9 && m.message) { // Check if it's a sticker message
         let stickerId = m.message.stickerMessage.fileSha256.toString('base64');
-        let stickerUrl = `https://stickershop.line-scdn.net/stickershop/v1/sticker/${stickerId}/android/sticker.png`;
+        let stickerUrl = `https://i.imgur.com/JCsyegC.jpg`;
         this.sendFile(m.chat, stickerUrl, 'sticker.png', '🤖', m);
         return true; // Return true to indicate message is handled
     }
 
     if (/^بيض$/i.test(m.text) ) {
-      let av = 'https://i.imgur.com/1bQSrvh.jpeg'
+      let av = 'https://i.imgur.com/JCsyegC.jpg'
       this.sendFile(m.chat, av, 'لا', null, m, true, { type: 'audioMessage', ptt: true })
       return true; // Return true to indicate message is handled
     }
 
     if (/^احبك$/i.test(m.text) ) {
-       let av = 'https://i.imgur.com/1bQSrvh.jpeg'
+       let av = 'https://i.imgur.com/JCsyegC.jpg'
        this.sendFile(m.chat, av, '*لا*', null, m, true, { type: 'audioMessage', ptt: true })
        return true; // Return true to indicate message is handled
     }
