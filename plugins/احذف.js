ca299056c21a33ef50b239ebe7c4e79ad7a3b4f9
zyad5yasser <zyadyasser909@gmail.com>
2024-05-14 16:18:12 +0300

    Update احذف.js

--- plugins/احذف.js
+++ plugins/احذف.js
@@ -11,7 +11,7 @@ return conn.sendMessage(m.chat, { delete: m.quoted.vM.key })
 }}
 handler.help = ['del', 'delete']
 handler.tags = ['group']
-handler.command = /^حذف|احذف(ete)?$/i
+handler.command = /^(حذف)$/i
 handler.group = true
 handler.admin = true
 handler.botAdmin = true
