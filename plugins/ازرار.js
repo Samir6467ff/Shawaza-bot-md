88eef551f79b9516457e4bbda98ded328bb9f6a5
zyad5yasser <zyadyasser909@gmail.com>
2024-05-15 00:13:19 +0300

    Update ازرار.js

--- plugins/ازرار.js
+++ plugins/ازرار.js
@@ -1,4 +1,4 @@
-let handler = async (m, { conn, args, usedPrefix, command }) => {
+let  handler = async (m, { conn, args, usedPrefix, command }) => {
     conn.relayMessage(m.chat, {
       viewOnceMessage: {
         message: {
@@ -40,93 +40,6 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
                   name: 'single_select',
                   buttonParamsJson: JSON.stringify({
                     title: 'اضغط',
-                    sections: [
-                      {
-                        title: 'قسم الالعاب',
-                        highlight_label: 'new',
-                        rows: [
-                          {
-                            header: 'info',
-                            title: '⌬ ❛╏',
-                            description: '',
-                            id: '.المطور'
-                          }, 
-                          {
-                                header: 'العاب', 
-                                title:'⌬ ❛╏زواج', 
-                                description: '', 
-                                id: '.زواج', 
-                         }, 
-                            {
-                                header: 'العاب', 
-                                title: '⌬ ❛╏لو', 
-                                description: '', 
-                                id: '.لو', 
-                         }, 
-                            {
-                                header: 'العاب', 
-                                title: '⌬ ❛╏طلاق', 
-                                description: '', 
-                                id: '.طلاق', 
-                         }, 
-                          
-                            {
-                                header: 'العاب', 
-                                title: '⌬ ❛╏صراحه', 
-                                description: '', 
-                                id: '.صراحه', 
-                         }, 
-                      
-                            {
-                                header: 'العاب', 
-                                title: '⌬ ❛╏احزر', 
-                                description: '', 
-                                id: '.احزر', 
-                         }, 
-
-                            {
-                                header: 'العاب', 
-                                title: '⌬ ❛╏خمن', 
-                                description: '', 
-                                id: '.خمن', 
-                         }, 
-
-                            {
-                                header: 'العاب', 
-                                title: '⌬ ❛╏تاج', 
-                                description: '', 
-                                id: '.تاج', 
-                         }, 
-                          
-                            {
-                                header: 'العاب', 
-                                title: '⌬ ❛╏اقتباس', 
-                                description: '', 
-                                id: '.اقتباس', 
-                         }, 
-
-
-                        ]
-                      }
-                    ]
-                  }),
-                  messageParamsJson:''
-                }
-              ]
-            }
-          }
-        }
-      }
-    },{})
-
-},
-    {
-                nativeFlowMessage: {
-              buttons: [
-                {
-                  name: 'single_select',
-                  buttonParamsJson: JSON.stringify({
-                    title: 'دوس',
                     sections: [
                       {
                         title: 'قسم الالعاب',
