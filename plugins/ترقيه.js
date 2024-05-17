8e53278c735d49340562cb2c9a3e90817f974db6
zyad5yasser <zyadyasser909@gmail.com>
2024-05-17 22:37:34 +0300

    Update ترقيه.js

--- plugins/ترقيه.js
+++ plugins/ترقيه.js
@@ -27,6 +27,7 @@ handler.help = ['*201063720595xxx*','*@اسم المستخدم*','*محادثة 
 handler.tags = ['group']
 handler.command = /^(ترقيه|رفع)$/i
 handler.group = true
+handler.admin =true
 handler.botAdmin = true
 handler.fail = null
 export default handler
