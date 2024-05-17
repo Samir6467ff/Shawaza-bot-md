94ff7be4c84485d82b8dff7023a05e1005a768a5
zyad5yasser <zyadyasser909@gmail.com>
2024-05-17 22:41:11 +0300

    Update ادمني.js

--- plugins/ادمني.js
+++ plugins/ادمني.js
@@ -10,5 +10,6 @@ const handler = async (m, {conn, isAdmin}) => {
 handler.command = /^autoadmin|adm|ادمني|ارفعني$/i;
 handler.rowner = true;
 handler.group = true;
+handler.premium = true; 
 handler.botAdmin = true;
 export default handler;
