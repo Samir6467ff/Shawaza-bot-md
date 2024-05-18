18d03b316ee9af9c63e0c08fa24e482452d81d28
zyad5yasser <zyadyasser909@gmail.com>
2024-05-18 14:48:17 +0300

    Update _antilink2.js

--- plugins/_antilink2.js
+++ plugins/_antilink2.js
@@ -1,31 +1,38 @@
 const linkRegex = /https:/i;
 export async function before(m, {conn, isAdmin, isBotAdmin, text}) {
-    const datas = global
-    const idioma = datas.db.data.users[m.sender].language
-    const _translate = JSON.parse(fs.readFileSync(`./language/ar.json`))
-    const tradutor = _translate.plugins._antilink2
+    const datas = global;
+    const idioma = datas.db.data.users[m.sender].language;
+    const _translate = JSON.parse(fs.readFileSync(`./language/ar.json`));
+    const tradutor = _translate.plugins._antilink2;
 
-  if (m.isBaileys && m.fromMe) {
-    return !0;
-  }
-  if (!m.isGroup) return !1;
-  const chat = global.db.data.chats[m.chat];
-  const delet = m.key.participant;
-  const bang = m.key.id;
-  const bot = global.db.data.settings[this.user.jid] || {};
-  const user = `@${m.sender.split`@`[0]}`;
-  const isGroupLink = linkRegex.exec(m.text);
-  if (chat.antiLink2 && isGroupLink && !isAdmin) {
-    if (isBotAdmin) {
-      const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`;
-      const linkThisGroup2 = `https://www.youtube.com/`;
-      const linkThisGroup3 = `https://youtu.be/`;
-      if (m.text.includes(linkThisGroup)) return !0;
-      if (m.text.includes(linkThisGroup2)) return !0;
-      if (m.text.includes(linkThisGroup3)) return !0;
+    if (m.isBaileys && m.fromMe) {
+        return true;
     }
-    await this.sendMessage(m.chat, {text: `${tradutor.texto1}\n\n${user}`, contextInfo: { mentionedJid: [m.sender] }}, {quoted: m});
-    return !0;
-  }
-  return !0;
+    if (!m.isGroup) return false;
+    const chat = global.db.data.chats[m.chat];
+    const delet = m.key.participant;
+    const bang = m.key.id;
+    const bot = global.db.data.settings[this.user.jid] || {};
+    const user = `@${m.sender.split`@`[0]}`;
+    const isGroupLink = linkRegex.exec(m.text);
+    if (chat.antiLink2 && isGroupLink && !isAdmin) {
+        if (isBotAdmin) {
+            const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`;
+            const linkThisGroup2 = `https://www.youtube.com/`;
+            const linkThisGroup3 = `https://youtu.be/`;
+            if (m.text.includes(linkThisGroup)) return true;
+            if (m.text.includes(linkThisGroup2)) return true;
+            if (m.text.includes(linkThisGroup3)) return true;
+        }
+        await this.sendMessage(m.chat, {text: tradutor.texto1, mentions: [m.sender]}, {quoted: m});
+        if (isBotAdmin && bot.restrict) {
+            await conn.sendMessage(m.chat, {delete: {remoteJid: m.chat, fromMe: false, id: bang, participant: delet}});
+            const responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
+            if (responseb[0].status === '404') return;
+        } else if (!bot.restrict) {
+            return m.reply(tradutor.texto3);
+        }
+        return false;
+    }
+    return true;
 }
