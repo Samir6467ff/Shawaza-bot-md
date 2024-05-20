f1cd8315b341fcf5224858eaddad3e3287dc3886
zyad5yasser <zyadyasser909@gmail.com>
2024-05-20 04:20:28 +0300

    Update تطقيم ولاد.js

--- plugins/تطقيم ولاد.js
+++ plugins/تطقيم ولاد.js
@@ -1,7 +1,7 @@
 import fetch from "node-fetch"
 let handler = async (m, { conn }) => {
 
-  let data = await (await fetch('src/game/طقم2.json')).json()
+  let data = await (await fetch('https://github.com/zyad5yasser/zezo-bot-md/blob/master/src/game/%D8%B7%D9%82%D9%852.json')).json()
   let cita = data[Math.floor(Math.random() * data.length)]
 
   let cowi = await(await fetch(cita.cowo)).buffer()
