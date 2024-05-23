f229959e39eab4d1b54807e64fc8602120c258a4
zyad5yasser <zyadyasser909@gmail.com>
2024-05-23 22:27:21 +0300

    Update قط.js

--- plugins/قط.js
+++ plugins/قط.js
@@ -4,6 +4,7 @@ const handler = async (m, {conn, text}) => {
     const res = await fetch('https://api.thecatapi.com/v1/images/search');
     const img = await res.json();
     const caption = `
+    *⌬ ❛╏ مياو مياو*
 *_⌬ ❛╏ BY: 𝒁𝒆𝒛𝒐 𝑩𝒐𝒕_*
 `.trim();
     conn.sendFile(m.chat, img[0].url, 'cat.jpg', caption, m);
@@ -12,8 +13,8 @@ const handler = async (m, {conn, text}) => {
     throw '*⌬ ❛╏خطأ!*';
   }
 };
-handler.help = ['cat'];
+handler.help = ['قط'];
 handler.tags = ['random'];
-handler.command = /^cat$/i;
+handler.command = /^قط$/i;
 handler.fail = null;
 export default handler;
