7b943f97032ba6d660c15bdb461fd41672b7c8bd
zyad5yasser <zyadyasser909@gmail.com>
2024-05-25 02:20:38 +0300

    Update بوت.js

--- plugins/بوت.js
+++ plugins/بوت.js
@@ -6,7 +6,7 @@ handler.all = async function(m) {
   //const s = seconds: '4556'
   // const estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: {orderMessage: { itemCount : -999999, status: 1, surface : 1, message: '𝑇ℎ𝑒 𝑀𝑦𝑠𝑡𝑖𝑐 - 𝐵𝑜𝑡', orderTitle: 'Bang', thumbnail: fs.readFileSync('./Menu2.jpg'), sellerJid: '0@s.whatsapp.net'}}}
   // const estiloaudio = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: {"audioMessage": { "mimetype":"audio/ogg; codecs=opus", "seconds": "99569", "ptt": "true"}}}
-  if (/^bot$/i.test(m.text) && !chat.isBanned) {
+  if (/^علاوي$/i.test(m.text) && !chat.isBanned) {
     conn.sendPresenceUpdate('recording', m.chat);
     await m.reply(`*علاوي حبيب قلبي*`);
     m.conn.sendMessage(m.chat, {audio: {url: vn}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
