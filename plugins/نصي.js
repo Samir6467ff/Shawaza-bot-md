2a99fcd30108d52177441a32905e9ce91f6d2263
zyad5yasser <zyadyasser909@gmail.com>
2024-05-26 01:41:46 +0300

    Update نصي.js

--- plugins/نصي.js
+++ plugins/نصي.js
@@ -1,77 +1,62 @@
-import { format } from 'util'
-// let path = require('path')
-import { spawn } from 'child_process'
+import { format } from 'util';
+import { spawn } from 'child_process';
 
-let fontPath = 'src/font/Zahraaa.ttf'
+const fontPath = 'src/font/Zahraaa.ttf';
+const inputPath = 'src/kertas/magernulis1.jpg';
+
 let handler = async (m, { conn, args }) => {
-    if (!global.support.convert &&
-        !global.support.magick &&
-        !global.support.gm) return handler.disabled = true // Disable if doesnt support
-    let inputPath = 'src/kertas/magernulis1.jpg'
-    let d = new Date
-    let tgl = d.toLocaleDateString('id-Id')
-    let hari = d.toLocaleDateString('id-Id', { weekday: 'long' })
-    let teks = args.join` `
-    // conn.reply(m.chat, util.format({fontPath, inputPath, outputPath, tgl, hari, teks}), m)
-    let bufs = []
-    const [_spawnprocess, ..._spawnargs] = [...(global.support.gm ? ['gm'] : global.support.magick ? ['magick'] : []),
+    if (!global.support.convert && !global.support.magick && !global.support.gm) {
+        handler.disabled = true; // Disable if not supported
+        return;
+    }
+
+    const d = new Date();
+    const tgl = d.toLocaleDateString('id-ID');
+    const hari = d.toLocaleDateString('id-ID', { weekday: 'long' });
+    const teks = args.join(' ');
+
+    const bufs = [];
+    const [_spawnprocess, ..._spawnargs] = [
+        ...(global.support.gm ? ['gm'] : global.support.magick ? ['magick'] : []),
         'convert',
         inputPath,
-        '-font',
-        fontPath,
-        '-fill',
-        'blue',
-        '-size',
-        '1024x784',
-        '-pointsize',
-        '20',
-        '-interline-spacing',
-        '1',
-        '-annotate',
-        '+806+78',
-        hari,
-        '-font',
-        fontPath,
-        '-fill',
-        'blue',
-        '-size',
-        '1024x784',
-        '-pointsize',
-        '18',
-        '-interline-spacing',
-        '1',
-        '-annotate',
-        '+806+102',
-        tgl,
-        '-font',
-        fontPath,
-        '-fill',
-        'blue',
-        '-size',
-        '1024x784',
-        '-pointsize',
-        '20',
-        '-interline-spacing',
-        '-7.5',
-        '-annotate',
-        '+344+142',
-        teks,
+        '-font', fontPath,
+        '-fill', 'blue',
+        '-size', '1024x784',
+        '-pointsize', '20',
+        '-interline-spacing', '1',
+        '-annotate', '+806+78', hari,
+        '-font', fontPath,
+        '-fill', 'blue',
+        '-size', '1024x784',
+        '-pointsize', '18',
+        '-interline-spacing', '1',
+        '-annotate', '+806+102', tgl,
+        '-font', fontPath,
+        '-fill', 'blue',
+        '-size', '1024x784',
+        '-pointsize', '20',
+        '-interline-spacing', '-7.5',
+        '-annotate', '+344+142', teks,
         'jpg:-'
-    ]
-    spawn(_spawnprocess, _spawnargs)
-        .on('error', e => m.reply(format(e)))
-        .on('close', () => {
-        m.reply('*⏳Aɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ... ฅ^•ﻌ•^ฅ⏳*')
-            conn.sendFile(m.chat, Buffer.concat(bufs), 'txt.jpg', '✅ Es mejor de lo que escribes tú ✍🏻', fakemsg)
-        })
-        .stdout.on('data', chunk => bufs.push(chunk))
-}
-handler.help = ['txt *<texto>*']
-handler.tags = ['fun']
-handler.command = ['escribe', 'txt','نصي'] 
-handler.register = true
+    ];
 
-export default handler
+    const process = spawn(_spawnprocess, _spawnargs);
+
+    process.on('error', e => m.reply(format(e)));
+    process.stdout.on('data', chunk => bufs.push(chunk));
+    process.on('close', () => {
+        m.reply('*⏳Aɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ... ฅ^•ﻌ•^ฅ⏳*');
+        conn.sendFile(m.chat, Buffer.concat(bufs), 'txt.jpg', '✅ Es mejor de lo que escribes tú ✍🏻');
+    });
+};
+
+handler.help = ['txt *<texto>*'];
+handler.tags = ['fun'];
+handler.command = ['escribe', 'txt'];
+handler.register = true;
+
+export default handler;
 
 // BY MFARELS
 // https://GitHub.com/MFarelS/
