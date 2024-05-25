a1a21ed8713a099aaa5afd225608e85fce0566cb
zyad5yasser <zyadyasser909@gmail.com>
2024-05-25 03:43:48 +0300

    Create بلوك فك بلوك.js

---
+++ plugins/بلوك فك بلوك.js
@@ -0,0 +1,32 @@
+
+const handler = async (m, {text, conn, usedPrefix, command}) => {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./language/ar.json`))
+  const tradutor = _translate.plugins.owner_block_unblock
+
+  const why = `${tradutor.texto1} ${usedPrefix + command} @${m.sender.split('@')[0]}*`;
+  const who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : false;
+  if (!who) conn.reply(m.chat, why, m, {mentions: [m.sender]});
+  const res = [];
+  switch (command) {
+    case 'blok': case 'block': case 'بلوك': 
+      if (who) {
+        await conn.updateBlockStatus(who, 'block').then(() => {
+          res.push(who);
+        });
+      } else conn.reply(m.chat, why, m, {mentions: [m.sender]});
+      break;
+    case 'بلوك_فك': case 'unblock': case 'فك_بلوك'
+      if (who) {
+        await conn.updateBlockStatus(who, 'unblock').then(() => {
+          res.push(who);
+        });
+      } else conn.reply(m.chat, why, m, {mentions: [m.sender]});
+      break;
+  }
+  if (res[0]) conn.reply(m.chat, `${tradutor.texto2[0]} ${command} ${tradutor.texto2[1]} ${res ? `${res.map((v) => '@' + v.split('@')[0])}` : ''}*`, m, {mentions: res});
+};
+handler.command = /^(block|unblock|بلوك|فك_بلوك|بلوك_فك)$/i;
+handler.rowner = true;
+export default handler;
