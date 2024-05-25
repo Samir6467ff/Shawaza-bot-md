10f652108f5a9bab550ebf4f539aad050bfc4c1a
zyad5yasser <zyadyasser909@gmail.com>
2024-05-25 23:47:29 +0300

    Update زواج.js

--- plugins/زواج.js
+++ plugins/زواج.js
@@ -1,4 +1,4 @@
-let toM = a => '@' + a.split('@')[0]
+/*let toM = a => '@' + a.split('@')[0]
 function handler(m, { groupMetadata }) {
 let ps = groupMetadata.participants.map(v => v.id)
 let a = ps.getRandom()
@@ -13,4 +13,4 @@ handler.help = ['formarpareja']
 handler.tags = ['main', 'fun']
 handler.command = ['formarpareja','زواج']
 handler.group = true
-export default handler
+export default handler*/
