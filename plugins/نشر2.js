17ca508d1578db30b088159e2919c9954e0eac92
zyad5yasser <zyadyasser909@gmail.com>
2024-05-26 03:01:54 +0300

    Update نشر2.js

--- plugins/نشر2.js
+++ plugins/نشر2.js
@@ -3,7 +3,7 @@ import fs from 'fs'
 let handler = async (m, { conn, text } ) => {
   if (!text) throw 'الرجاء إدخال نص لإرسال الإعلان إلى جميع المجموعات'
   let groups = Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats && !chat.metadata?.read_only && !chat.metadata?.announce).map(v => v[0])
-  let fproducto = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "17608914335@s.whatsapp.net" } : {}) }, message: { "productMessage": { "product": { "productImage":{ "mimetype": "image/jpeg", "jpegThumbnail": catalogo }, "title": `🐢𝐀𝐍𝐔𝐍𝐂𝐈𝐎 𝐃𝐄 𝐆𝐑𝐔𝐏𝐎🐢`, "description": "NyanCatBot-MD", "currencyCode": "USD", "priceAmount1000": "200000000", "retailerId": "Ghost", "productImageCount": 1 }, "businessOwnerJid": `0@s.whatsapp.net` }}}
+  let fproducto = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "17608914335@s.whatsapp.net" } : {}) }, message: { "productMessage": { "product": { "productImage":{ "mimetype": "image/jpeg", "jpegThumbnail": imagen4 }, "title": `🐢𝐀𝐍𝐔𝐍𝐂𝐈𝐎 𝐃𝐄 𝐆𝐑𝐔𝐏𝐎🐢`, "description": "NyanCatBot-MD", "currencyCode": "USD", "priceAmount1000": "200000000", "retailerId": "Ghost", "productImageCount": 1 }, "businessOwnerJid": `0@s.whatsapp.net` }}}
   let teks = `\t\t\t\t*إعلان | المجموعات*\n\n${text}`
   for (let id of groups) {
   await conn.sendMessage(id, { text: teks, mentions: (await conn.groupMetadata(`${id}`)).participants.map(v => v.id) }, { quoted: fproducto })
