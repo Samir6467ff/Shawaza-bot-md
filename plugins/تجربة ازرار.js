632a95069936b5fc1414929e5237694a4e0d2019
zyad5yasser <zyadyasser909@gmail.com>
2024-05-27 03:17:26 +0300

    Update تجربة ازرار.js

--- plugins/تجربة ازرار.js
+++ plugins/تجربة ازرار.js
@@ -1,86 +1,110 @@
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-  const taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-  const time = moment.tz('Africa/Egypt').format('HH')
-  let wib = moment.tz('Africa/Egypt').format('HH:mm:ss')
-  let date = d.toLocaleDateString(locale, { day: 'numeric', month: 'long', year: 'numeric' })
+const taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
+const time = moment.tz('Africa/Egypt').format('HH')
+let wib = moment.tz('Africa/Egypt').format('HH:mm:ss')
+let date = new Date().toLocaleDateString('en-EG', { day: 'numeric', month: 'long', year: 'numeric' }); 
+await conn.sendMessage(m.chat, { react: { text: '📂', key: m.key } })
 
+   
   conn.relayMessage(m.chat, {
-    viewOnceMessage: {
-      message: {
-        interactiveMessage: {
-          header: {
-            title: `┈┈┈┈┈⟢┈┈┈⟣┈┈┈┈┈┈┈⟢
+      viewOnceMessage: {
+        message: {
+          interactiveMessage: {
+            header: {
+              title: `┈┈┈┈┈⟢┈┈┈⟣┈┈┈┈┈┈┈⟢
 *🐉✬⃝╿↵ مرحــبـا ⌊ ${m.pushName} ⌉*
 ── • ◈ • ──
-https://telegra.ph/file/aa3da4c558a6975817eb7.png`,
-            image: {
-              link: 'https://telegra.ph/file/aa3da4c558a6975817eb7.png',
-                  }
-          },
-          body: {
-            text: '> اذا واجهتك مشكله اكتب ابلاغ واكتب رسالتك\n> ➳ᴹᴿ᭄𝒁𝒆𝒛𝒐➳ᴹᴿ᭄'
-          },
-          nativeFlowMessage: {
-            buttons: [
-              {
-                name: 'single_select',
-                buttonParamsJson: JSON.stringify({
-                  title: 'اضغط',
-                  sections: [
-                    {
-                      title: 'قوائم',
-                      highlight_label: 'new',
-                      rows: [
-                        {
-                          header: 'info',
-                          title: '⌬ ❛╏المطور',
-                          description: '',
-                          id: '.المطور'
-                        },
-                        {
-                          header: 'قوائم',
-                          title: '⌬ ❛╏التنزيلات',
-                          description: '',
-                          id: '.4',
-                        },
-                        {
-                          header: 'قوائم',
-                          title: '⌬ ❛╏قائمه الجروب',
-                          description: '',
-                          id: '.5',
-                        },
-                        {
-                          header: 'قوائم',
-                          title: '⌬ ❛╏الالعاب',
-                          description: '',
-                          id: '.6',
-                        },
 
-                        {
-                          header: 'قوائم',
-                          title: '⌬ ❛╏الالعاب',
-                          description: '',
-                          id: '.6',
-                        },
+┏━━🤖 _مـعلـومـات البـوت:_🤖━━┓
+┃ ✨  *اسـم البـوت: 𝒁𝒆𝒛𝒐 𝑩𝒐𝒕*
+┃ 💻  *المـنصـة:* 𝑯𝑬𝑹𝑶𝑲𝑼💀 
+┃ 📍  *رقم المطور: 201508628077*
+┃ 📚  *اسم المطور: 『➳ᴹᴿ᭄𝒁𝒆𝒛𝒐➳ᴹᴿ᭄』* 
+┗━━━━━━━━━━━━━┛
 
-                        {
-                          header: 'كل الاوامر',
-                          title: '⌬ ❛╏قائمة الاوامر',
-                          description: '',
-                          id: '.10',
-                        },
-                      ]
-                    }
-                  ]
-                }),
-                messageParamsJson: 'ZEZO bot'
-              }
-            ]
+┏━━⏰ _الـتـاريـخ والـوقـت!_ ⏰━┓
+┃ 📆  *تـاريـخ اليـوم:* ${date} 
+┃ ⏲️  *الـوقـت الـحالـي:* ${wib} 
+┗━━━━━━━━━━━━━┛
+⟣┈┈┈┈┈┈⟢┈┈┈⟣┈┈┈┈┈┈┈⟢`
+            },
+            body: {
+              text: '> اذا واجهتك مشكله اكتب ابلاغ واكتب رسالتك\n> ➳ᴹᴿ᭄𝒁𝒆𝒛𝒐➳ᴹᴿ᭄'
+            },
+            nativeFlowMessage: {
+              buttons: [
+                
+                {
+                  name: 'single_select',
+                  buttonParamsJson: JSON.stringify({
+                    title: '『』CLICK《',
+                {
+                  name: 'single_select',
+                  buttonParamsJson: JSON.stringify({
+                    title: '『』CLICK《',
+                    sections: [
+                      {
+                        title: '『』MENUS《',
+                        highlight_label: 'OWNER', 
+                        rows: [
+                          {
+                            header: 'info',
+                            title: '⌬ ❛╏المطور',
+                            description: '',
+                            id: '.المطور'
+                          },
+                           {
+                                header: '『』MENU《', 
+                                title:'⌬ ❛╏التنزيلات', 
+                                description: '', 
+                                id: '.4', 
+                         }, 
+                            {
+                                header: '『』MENU《', 
+                                title: '⌬ ❛╏قائمه الجروب', 
+                                description: '', 
+                                id: '.5', 
+                         }, 
+                            {
+                                header: '『』MENU《', 
+                                title: '⌬ ❛╏الالعاب', 
+                                description: '', 
+                                id: '.6', 
+                         }, 
+                            
+                            {
+                                header: '『』MENU《', 
+                                title: '⌬ ❛╏الترفيه', 
+                                description: '', 
+                                id: '.6', 
+                         }, 
+                           
+                            {
+                                header: '『』MENU《', 
+                                title: '⌬ ❛╏الصور', 
+                                description: '', 
+                                id: '.2', 
+                         }, 
+                            
+                            {
+                                header: '『』All MENU《', 
+                                title: '⌬ ❛╏قائمة الاوامر', 
+                                description: '', 
+                                id: '.10', 
+                         }, 
+                        ]
+                      }
+                    ]
+                  }),
+                  messageParamsJson:'ZEZO bot'
+                }
+              ]
+            }
           }
         }
       }
-    }
-  }, {})
+    }, {})
+
 }
 
 handler.help = ['info']
