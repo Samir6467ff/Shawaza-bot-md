69c13d81dae4671a265ef8c54ccec120313efa43
zyad5yasser <zyadyasser909@gmail.com>
2024-05-28 14:56:52 +0300

    Update الدعم.js

--- plugins/الدعم.js
+++ plugins/الدعم.js
@@ -1,9 +1,19 @@
-import axios from "axios"
-let handler = async (m, {command, conn, usedPrefix}) => {
-//let res = (await axios.get(`https://raw.githubusercontent.com/socona12/TheMystic-Bot-MD/master/src/JSON/anime-Venom.json`)).data  
-//let haha = await res[Math.floor(res.length * Math.random())]  
-const taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-conn.sendFile(m.chat,imagen4,`
+import axios from "axios";
+
+let handler = async (m, { command, conn, usedPrefix }) => {
+  // جلب بيانات JSON من الرابط المحدد
+  let res = (await axios.get(`https://raw.githubusercontent.com/socona12/TheMystic-Bot-MD/master/src/JSON/anime-Venom.json`)).data;  
+  let haha = await res[Math.floor(res.length * Math.random())];  
+  const taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
+  
+  // استخدم حقل الصورة من البيانات التي تم جلبها
+//  let imagen4 = haha.image;
+
+  // إرسال الصورة
+  await conn.sendFile(m.chat, imagen4, 'image.jpg', '');
+
+  // إرسال الرسالة النصية
+  conn.sendMessage(m.chat, `
 *◉═══ • ❁ 『』WELCOME 《 ❁ • ═══◉*
 WELCOME ➳『 ${taguser} 』
 *『 ️اليك قائمه بمعلومات المطور  』*
@@ -21,8 +31,10 @@ WELCOME ➳『 ${taguser} 』
 *⊱≼ https://chat.whatsapp.com/JO7neq006uI3OgEtjNvtm0 ≽⊰⊹*
 *『』𝒁𝒆𝒛𝒐 𝑩𝒐𝒕《*
 *◉═══ • ❁ BAY ❁ • ═══◉* 
- `, m)
+  `, 'conversation');
 }
-handler.command = handler.help = ['معلومات','الدعم']
-handler.tags = ['kaneki']
-export default handler
+
+handler.command = handler.help = ['معلومات', 'الدعم'];
+handler.tags = ['kaneki'];
+
+export default handler;
