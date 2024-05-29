abf24e189aebc7424345c2957c88824efbd98a7e
zyad5yasser <zyadyasser909@gmail.com>
2024-05-29 10:21:15 +0300

    Update اختفاء.js

--- plugins/اختفاء.js
+++ plugins/اختفاء.js
@@ -1,9 +1,7 @@
 import _translate from "./_translate.js";
-const idioma = datas.db.data.users[m.sender].language
-const _translate = JSON.parse(fs.readFileSync(`./language/ar.json`))
-const tradutor = _translate.plugins.gc_add
+const tradutor = _translate.plugins.afk_afk
  // Para configurar o idioma, na raiz do projeto altere o arquivo config.json
-  // Para configurar el idioma, en la raíz del proyecto, modifique el archivo config.json.
+  // Para configurar el idioma, en la raÃ­z del proyecto, modifique el archivo config.json.
   // To set the language, in the root of the project, modify the config.json file.
 
 const handler = async (m, {text}) => {
