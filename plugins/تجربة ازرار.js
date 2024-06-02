de84c174de9b66fd3066cb4a077a2cb8de33de71
zyad5yasser <zyadyasser909@gmail.com>
2024-06-02 12:42:19 +0300

    Update تجربة ازرار.js

--- plugins/تجربة ازرار.js
+++ plugins/تجربة ازرار.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     await conn.sendMessage(m.chat, { react: { text: '📂', key: m.key } });
 
     // تحميل الصورة كـ buffer
-    const imageBuffer = await fetch('').then(res => res.buffer());
+    const imageBuffer = await fetch('https://telegra.ph/file/2bcdd8e6cc7a486803d88.jpg').then(res => res.buffer());
 
     conn.relayMessage(m.chat, {
         viewOnceMessage: {
