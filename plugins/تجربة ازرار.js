a733f26a1dffa2927068c50f067355019e303d1f
zyad5yasser <zyadyasser909@gmail.com>
2024-06-03 15:21:38 +0300

    Update تجربة ازرار.js

--- plugins/تجربة ازرار.js
+++ plugins/تجربة ازرار.js
@@ -6,16 +6,25 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 
     await conn.sendMessage(m.chat, { react: { text: '📂', key: m.key } });
 
-    // Fetch the image and send it as an image message
-    const imageBuffer = await fetch('https://telegra.ph/file/2bcdd8e6cc7a486803d88.jpg').then(res => res.buffer())
+    // Fetch the image and convert it to a buffer
+    const imageBuffer = await fetch('https://telegra.ph/file/2bcdd8e6cc7a486803d88.jpg').then(res => res.buffer());
 
-    // Send the interactive message
+    // Prepare the buttons
+    const buttons = [
+        { buttonId: '.المطور', buttonText: { displayText: '⌬ ❛╏المطور' }, type: 1 },
+        { buttonId: '.4', buttonText: { displayText: '⌬ ❛╏التنزيلات' }, type: 1 },
+        { buttonId: '.5', buttonText: { displayText: '⌬ ❛╏قائمه الجروب' }, type: 1 },
+        { buttonId: '.6', buttonText: { displayText: '⌬ ❛╏الالعاب' }, type: 1 },
+        { buttonId: '.6', buttonText: { displayText: '⌬ ❛╏الترفيه' }, type: 1 },
+        { buttonId: '.2', buttonText: { displayText: '⌬ ❛╏الصور' }, type: 1 },
+        { buttonId: '.20', buttonText: { displayText: '⌬ ❛╏شروط' }, type: 1 },
+        { buttonId: '.الدعم', buttonText: { displayText: '⌬ ❛╏الدعم' }, type: 1 },
+        { buttonId: '.10', buttonText: { displayText: '⌬ ❛╏قائمة الاوامر' }, type: 1 },
+    ];
+
+    // Prepare the message content
     const buttonMessage = {
-        viewOnceMessage: {
-            message: {
-                imageMessage: {
-                    image: imageBuffer, // The image to be sent
-                    caption: `┈┈┈┈┈⟢┈┈┈⟣┈┈┈┈┈┈┈⟢        
+        caption: `┈┈┈┈┈⟢┈┈┈⟣┈┈┈┈┈┈┈⟢        
 *🐉✬⃝╿↵ مرحــبـا ⌊ ${m.pushName} ⌉*
 ── • ◈ • ──
 
@@ -31,57 +40,9 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 ┃ ⏲️  *الـوقـت الـحالـي:* 『』${wib}《 
 ┗━━━━━━━━━━━━━┛
 ⟣┈┈┈┈┈┈⟢┈┈┈⟣┈┈┈┈┈┈┈⟢`,
-                    buttons: [
-                        {
-                            buttonId: '.المطور',
-                            buttonText: { displayText: '⌬ ❛╏المطور' },
-                            type: 1
-                        },
-                        {
-                            buttonId: '.4',
-                            buttonText: { displayText: '⌬ ❛╏التنزيلات' },
-                            type: 1
-                        },
-                        {
-                            buttonId: '.5',
-                            buttonText: { displayText: '⌬ ❛╏قائمه الجروب' },
-                            type: 1
-                        },
-                        {
-                            buttonId: '.6',
-                            buttonText: { displayText: '⌬ ❛╏الالعاب' },
-                            type: 1
-                        },
-                        {
-                            buttonId: '.6',
-                            buttonText: { displayText: '⌬ ❛╏الترفيه' },
-                            type: 1
-                        },
-                        {
-                            buttonId: '.2',
-                            buttonText: { displayText: '⌬ ❛╏الصور' },
-                            type: 1
-                        },
-                        {
-                            buttonId: '.20',
-                            buttonText: { displayText: '⌬ ❛╏شروط' },
-                            type: 1
-                        },
-                        {
-                            buttonId: '.الدعم',
-                            buttonText: { displayText: '⌬ ❛╏الدعم' },
-                            type: 1
-                        },
-                        {
-                            buttonId: '.10',
-                            buttonText: { displayText: '⌬ ❛╏قائمة الاوامر' },
-                            type: 1
-                        }
-                    ],
-                    footerText: 'اذا واجهتك مشكله اكتب ابلاغ واكتب رسالتك\n ➳ᴹᴿ᭄𝒁𝒆𝒛𝒐➳ᴹᴿ᭄'
-                }
-            }
-        }
+        image: imageBuffer,
+        buttons: buttons,
+        footerText: 'اذا واجهتك مشكله اكتب ابلاغ واكتب رسالتك\n ➳ᴹᴿ᭄𝒁𝒆𝒛𝒐➳ᴹᴿ᭄'
     };
 
     await conn.sendMessage(m.chat, buttonMessage, { quoted: m });
