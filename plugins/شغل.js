67e111d40463d5e708304def87dcabd59b1b0d9d
zyad5yasser <zyadyasser909@gmail.com>
2024-06-08 01:57:58 +0300

    Update شغل.js

--- plugins/شغل.js
+++ plugins/شغل.js
@@ -80,7 +80,7 @@ var handler = async (m, { conn, command, text, usedPrefix }) => {
 
 handler.help = ['play'].map((v) => v + ' <query>');
 handler.tags = ['downloader'];
-handler.command = /^شغل$/i;
+handler.command = /^ابدأ|ابدا$/i;
 
 handler.exp = 0;
 handler.diamond = false;
