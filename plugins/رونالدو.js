a01b91ffe2ccd3882c32641d77a8df0fb4333076
zyad5yasser <zyadyasser909@gmail.com>
2024-06-08 12:33:50 +0300

    Update رونالدو.js

--- plugins/رونالدو.js
+++ plugins/رونالدو.js
@@ -4,23 +4,24 @@ let handler = async (m, { conn, usedPrefix }) => {
   try {
     // جلب بيانات كريستيانو رونالدو من الملف JSON
     const cristiano = (await axios.get('https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/CristianoRonaldo.json')).data;
-    const ronaldo = cristiano[Math.floor(cristiano.length * Math.random())];
+    const ronaldo = cristiano[Math.floor(cristiano.length * Math.random())].result;
 
     // إعداد الرسالة
     const buttons = [
-      { buttonId: `${usedPrefix}الدون`, buttonText: { displayText: 'الدون' }, type: 1 },
+      { buttonId: `${usedPrefix}الدون`, buttonText: { displayText: 'التالي' }, type: 1 },
       { buttonId: `${usedPrefix}الدعم`, buttonText: { displayText: 'الدعم' }, type: 1 }
     ];
 
     const buttonMessage = {
-      contentText: '*عمك ميسي*',
-      footerText: 'اختر أحد الخيارات:',
+      image: { url: ronaldo }, // استخدام URL الصورة مباشرة
+      caption: 'اختر أحد الخيارات:',
+      footer: 'اختر أحد الخيارات:',
       buttons: buttons,
-      headerType: 1
+      headerType: 4 // 4 تعني رسالة تحتوي على صورة وأزرار
     };
 
-    // إرسال الصورة
-    await conn.sendFile(m.chat, ronaldo, 'ronaldo.jpg', buttonMessage);
+    // إرسال الصورة مع الأزرار
+    await conn.sendMessage(m.chat, buttonMessage, { quoted: m });
   } catch (error) {
     console.error(error);
   }
