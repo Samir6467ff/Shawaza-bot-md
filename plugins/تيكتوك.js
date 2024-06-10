6f640741220f1dda9fb21b70e508086222d4e185
zyad5yasser <zyadyasser909@gmail.com>
2024-06-10 22:33:35 +0300

    Update تيكتوك.js

--- plugins/تيكتوك.js
+++ plugins/تيكتوك.js
@@ -1,5 +1,6 @@
 import fetch from 'node-fetch';
 import fs from 'fs';
+import { exec } from 'child_process';
 
 let handler = async (m, { conn, usedPrefix, args, command, text }) => {
   if (!text) throw `*يجب عليك إعطاء رابط أي فيديو أو صورة من TikTok*`;
@@ -11,6 +12,10 @@ let handler = async (m, { conn, usedPrefix, args, command, text }) => {
     if (!mediaURL) throw 'لم يتم العثور على فيديو للرابط المعطى';
 
     conn.sendFile(m.chat, mediaURL, '', 'هذا هو الفيديو ⚡', m, false, { mimetype: 'video/mp4' });
+
+    let audioFileName = await extractAudio(mediaURL);
+
+    conn.sendFile(m.chat, audioFileName, '', 'هذا هو المقطع الصوتي 🎵', m, false, { mimetype: 'audio/mpeg' });
   } catch (error) {
     throw `حدث خطأ: ${error.message}`;
   }
@@ -30,6 +35,19 @@ async function zoro(text) {
   });
 
   return fileName;
+}
+
+function extractAudio(videoFile) {
+  return new Promise((resolve, reject) => {
+    const audioFile = 'Zoro_tiktok_audio.mp3';
+    exec(`ffmpeg -i ${videoFile} -q:a 0 -map a ${audioFile}`, (error, stdout, stderr) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(audioFile);
+      }
+    });
+  });
 }
 
 handler.help = ['tiktok'];
