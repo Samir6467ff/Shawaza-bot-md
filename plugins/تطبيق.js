d716abeefe8c8ee902f78d732b853dc1255dc1b6
zyad5yasser <zyadyasser909@gmail.com>
2024-06-14 03:27:58 +0300

    Update تطبيق.js

--- plugins/تطبيق.js
+++ plugins/تطبيق.js
@@ -1,18 +1,6 @@
 import {search, download} from 'aptoide-scraper';
 const handler = async (m, {conn, usedPrefix: prefix, command, text}) => {
- if (!text) throw `*📥╎اكـتـب اسـم تـطـبـيـق لـتـنـزيـلـه🖇️*`;
-  try {    
-    const searchA = await search(text);
-    const data5 = await download(searchA[0].id);
-    let response = `*〖📲┇تـحـمـيـل الـتـطـبـيـقـات┇📲〗*\n\n*֎╎اسـم الـتـطبـيـق📌┇* ${data5.name}\n*֎╎اسـم الـطـرد📦┇* ${data5.package}\n*֎╎الانـتـاج🕒┇* ${data5.lastup}\n*֎╎الـحـجـم📥┇* ${data5.size}\n\nـــــــــــــــــــ☽ 𝑧ₑ𝑧ₒ_𝑏ₒ𝑡 ☾ـــــــــــــــــــــــــــ`
-    await conn.sendMessage(m.chat, {image: {url: data5.icon}, caption: response}, {quoted: m});
- if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
-      return await conn.sendMessage(m.chat, {text: '*֎╎حـجـم الـمـلـف كـبـيـر جـدا لـذا لـن يـتـم ارسـالـه⛔*'}, {quoted: m});
-    }
-    await conn.sendMessage(m.chat, {document: {url: data5.dllink}, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null}, {quoted: m});
-  } catch {
-    throw `*֎╎ايـرور📍*`;
-  }    
+m.reply (`*خد*\n*لا خد بجد*`) 
 };
 handler.command = /^(apkmod|apk|apek|تطبيق)$/i;
 export default handler;
