8c67a21fc04270cfa1c2dd17c4ce8783da52fbfc
zyad5yasser <zyadyasser909@gmail.com>
2024-06-15 16:48:18 +0300

    Update ايديت.js

--- plugins/ايديت.js
+++ plugins/ايديت.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch';
 
-const apiKey = 'AIzaSyAj0oG342v6Js1FzpK7HCqe6iMFeHM28Pw'; // استبدل هذه القيمة بمفتاح API الخاص بك
+const apiKey = 'AIzaSyAj0oG342v6Js1FzpK7HCqe6iMFeHM28Pw'; // تم تضمين مفتاح API الخاص بك
 
 async function searchYouTube(query) {
   const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&type=video&key=${apiKey}`);
@@ -10,21 +10,26 @@ async function searchYouTube(query) {
 
 let handler = async (m, { conn, text }) => {
   try {
-    const searchQuery = text || 'edit'; // استخدم النص المدخل كمصطلح بحث أو 'edit' كافتراضي
+    if (!text) {
+      await conn.sendMessage(m.chat, { text: 'يرجى تقديم النص الذي تريد البحث عنه.' }, { quoted: m });
+      return;
+    }
+
+    const searchQuery = text; // استخدم النص المدخل كمصطلح بحث
     const videos = await searchYouTube(searchQuery);
 
     if (videos.length > 0) {
       const randomVideo = videos[Math.floor(Math.random() * videos.length)];
       const videoUrl = `https://www.youtube.com/watch?v=${randomVideo.id.videoId}`;
 
-      await conn.sendFile(m.chat, videoUrl, 'dado.webp', '', m);
+      await conn.sendMessage(m.chat, { text: videoUrl }, { quoted: m });
       await conn.sendMessage(m.chat, { react: { text: '🎞', key: m.key } });
     } else {
-      await conn.sendMessage(m.chat, { text: 'لم يتم العثور على فيديوهات.' }, m);
+      await conn.sendMessage(m.chat, { text: 'لم يتم العثور على فيديوهات.' }, { quoted: m });
     }
   } catch (error) {
     console.error(error);
-    await conn.sendMessage(m.chat, { text: 'حدث خطأ أثناء البحث عن الفيديوهات.' }, m);
+    await conn.sendMessage(m.chat, { text: 'حدث خطأ أثناء البحث عن الفيديوهات.' }, { quoted: m });
   }
 };
 
