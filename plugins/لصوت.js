c22c116ab57faefd4ee32bf5db7eac0550c38103
zyad5yasser <zyadyasser909@gmail.com>
2024-06-15 17:52:45 +0300

    Update لصوت.js

--- plugins/لصوت.js
+++ plugins/لصوت.js
@@ -2,8 +2,8 @@ import { toAudio } from '../lib/converter.js'
 
 let handler = async (m, { conn, usedPrefix, command }) => {
     let q = m.quoted ? m.quoted : m
-   /* let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-    if (!/video|audio/.test(mime)) throw `✳️ Reply to the video or voice note you want to convert to mp3 with the command :\n\n*${usedPrefix + command}*`*/
+    let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
+    if (!/video|audio/.test(mime)) throw `*✳️ قم بالرد علي فبديو لتحوليه لصوت*`
     let media = await q.download?.()
     if (!media) throw '❎ فشل تحميل الوسائط'
     let audio = await toAudio(media, 'mp4')
@@ -12,6 +12,6 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 }
 handler.help = ['tomp3']
 handler.tags = ['fun']
-handler.command = /^ل(صوت|a(udio)?)$/i
+handler.command = /^(لصوت|لفويس|tomp3)$/i
 
 export default handler
