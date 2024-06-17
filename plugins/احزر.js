90e19ec0ac7d961e7fa1253b5b7a6045b32e937f
zyad5yasser <zyadyasser909@gmail.com>
2024-06-17 21:50:52 +0300

    Update احزر.js

--- plugins/احزر.js
+++ plugins/احزر.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn, command, usedPrefix }) => {
     }
     let src = await (await fetch('https://gist.githubusercontent.com/Kyutaka101/98d564d49cbf9b539fee19f744de7b26/raw/f2a3e68bbcdd2b06f9dbd5f30d70b9fda42fec14/guessflag')).json()
   let json = src[Math.floor(Math.random() * src.length)]
-    let caption = `*${command.toUpperCase()}*
+    let caption = `${command.toUpperCase()}
   ❐↞┇الـوقـت⏳↞ *${(timeout / 1000).toFixed(2)} ┇
   *استخدم .انسحب للأنسحاب*
   ❐↞┇الـجـائـزة💰↞ ${poin} نقاط┇
